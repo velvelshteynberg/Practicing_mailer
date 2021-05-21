af20098298ea1d769b9b84e877852d5c6095ed03
Velvelshteynberg <Velvelshteynberg@outlook.com>
2021-05-21 13:54:04 -0400

    working on sending out an email automatically from rails

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         # Tell the usermailer to send a welcome email after save
-        UserMailer.with(user: @user).welcome_email.deliver_now
+        UserMailer.with(user: @user).welcome_email.deliver_later
         format.html { redirect_to @user, notice: "User was successfully created." }
         format.json { render :show, status: :created, location: @user }
       else
